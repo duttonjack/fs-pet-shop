03ffa9aad4b7d1204c34678063b955f597ad2348
Jack Dutton <duttonjack@Jacks-MacBook-Pro.local>
2023-12-06 09:59:30 -0500

    finished implementation for read

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -1,5 +1,4 @@
-
-console.log(typeof process.argv[2], process.argv[2])
+const fs = require("fs")
 const acceptable = ["create", "destroy", "update", "read"]
 
 // Error Handling for Invalid Commands
@@ -10,3 +9,17 @@ if (!(acceptable.includes(process.argv[2]))){
     })
 }
 
+if (process.argv[2] === "read"){
+    let petIndex = process.argv[3]
+    fs.readFile('../pets.json', (err, data) => {
+        if (err) throw err;
+        let petArray = JSON.parse(data)
+
+        if (petIndex > petArray.length -1 || petIndex < 0){
+            console.error(`Usage: nodefs.js read INDEX`)
+            console.log("Acceptable index values: 0 to 1")
+        } else {
+            console.log(petArray[petIndex])
+        }
+    })
+}
