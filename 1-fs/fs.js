1522cfcc25e335e42766226a05c47eb23ebb950b
Jack Dutton <duttonjack@Jacks-MacBook-Pro.local>
2023-12-06 09:43:10 -0500

    Part 1 CLI Error handler complete

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -1,1 +1,12 @@
 
+console.log(typeof process.argv[2], process.argv[2])
+const acceptable = ["create", "destroy", "update", "read"]
+
+// Error Handling for Invalid Commands
+if (!(acceptable.includes(process.argv[2]))){ 
+    console.error("Usage: nodefs.js [read | create | update | destroy]")
+    process.on("exit", () => {
+        console.log('Process exited with code 5')
+    })
+}
+
