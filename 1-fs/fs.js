c85cc0c8b557ea0a1fb2b94d6ded002ca72efae9
Jack Dutton <duttonjack@Jacks-MacBook-Pro.local>
2023-12-06 10:33:24 -0500

    finished part 1

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -23,3 +23,35 @@ if (process.argv[2] === "read"){
         }
     })
 }
+
+if (process.argv[2] === "create"){
+  let age = process.argv[3]
+  let kind = process.argv[4]
+  let name = process.argv[5]
+  if (age && kind && name){
+    createFunction(Number(age), kind, name)
+  } else {
+    console.log("Usage: node fs.js create AGE KIND NAME")
+    process.on("exit", () => {
+        console.log('Process exited with code 4')
+    })
+  }
+}
+
+function createFunction(age, kind, name){
+  newPet = {
+    age: age,
+    kind: kind,
+    name: name
+  }
+  console.log(newPet)
+  fs.readFile('../pets.json', (err, data) => {
+    if (err) throw err;
+    let petArray = JSON.parse(data)
+    petArray.push(newPet)
+    console.log(petArray)
+    fs.writeFile('../pets.json', JSON.stringify(petArray), function(err){
+        if (err) throw err;
+    })
+  });
+}
